c4444446eee59474e3e17fbbd6ac63320f6db8b3
tink18lqd <hakhanh09110911@gmail.com>
2024-07-28 13:18:40 +0700

    Update 0 fix Bug (spam "Thx For visit")

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-var a = "Thanks For Visit My WebSite 🥰"
+var a = "Thanks For Visit My WebSite"
 d = 0
 var b = ""
-time_wait_for_appear_letter = 50
+times = 0
+time_wait_for_appear_letter = 25
+var text = a
 function congratulation(){
+    times += 1
+    b = ""
     // document.getElementById("clicked").innerHTML = "Thanks For Visit My WebSite 🥰"
     dequy()
+    if(times <= 1){
+        text = a + " 🥰"
+    }
+    if(times >= 2){
+        text = a + "👿 (You Have Clicked " + times.toString() + " Times)"
+    }
 }
 
 function dequy(){
-    if(d == a.length){
+    if(d == text.length){
         d = 0;
         return;
     }
     else{
-        if(a[d] == " "){
+        if(text[d] == " "){
             time_wait_for_appear_letter = 0
         }
-        b += a[d];
+        b += text[d];
         d += 1;
         document.getElementById("clicked").innerHTML = b;
         setTimeout(() => {
-            time_wait_for_appear_letter = 50
+            time_wait_for_appear_letter = 25
             dequy();
         }, time_wait_for_appear_letter);
     }
